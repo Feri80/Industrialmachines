b3bf36ed07840236568c9b3d03ad78fb77c7901a
Feri80 <bigfarhad7780@gmail.com>
2021-02-03 19:43:49 +0330

    delete_machine added

--- main.c
+++ main.c
@@ -71,13 +71,18 @@ void add_machine()
         fprintf(fo,"%s\n",new_machine.repairdescription);
         fclose(fo);
     }
-    
     sz++;
+    printf("Added Successfully \n");
 }
 
 int find_machine(int code)
 {
     FILE *fin=fopen("machines.dat","r");
+    if(fin==NULL)
+    {
+        printf("Can't Find Files \n");
+        return -1;
+    }
     int t;
     char c;
     int x;
@@ -114,6 +119,62 @@ void delete_machine()
     printf("Please Enter Your Machine Repair Code : ");
     int x;
     scanf("%d",&x);
+    int id=find_machine(x);
+    if(id==-1)
+    {
+        printf("Can't Find This Machine \n");
+        return;
+    }
+    struct machine machines[sz-1];
+    FILE *fin=fopen("machines.dat","r");
+    int t;
+    char c;
+    char b[maxn];
+    char tmp[maxn];
+    int j=0;
+    for(int i=0;i<sz;i++)
+    {
+        if(i!=id)
+        {
+            fscanf(fin,"%c",&c);
+            fscanf(fin,"%d",&t);
+            machines[j].code=t;
+            fscanf(fin,"%d",&t);
+            machines[j].repaircode=t;
+            fscanf(fin,"%d",&t);
+            machines[j].repairperiod=t;
+            fscanf(fin," %c",&c);
+            machines[j].repairtype=c;
+            fgets(tmp,maxn,fin);
+            fgets(b,maxn,fin);
+            strcpy(machines[j].repairdescription,b);
+            j++;
+        }
+        else
+        {
+            fscanf(fin,"%c",&c);
+            fscanf(fin,"%d",&t);
+            fscanf(fin,"%d",&t);
+            fscanf(fin,"%d",&t);
+            fscanf(fin," %c",&c);
+            fgets(tmp,maxn,fin);
+            fgets(b,maxn,fin);
+        }
+    }
+    fclose(fin);
+    sz--;
+    FILE *fo=fopen("machines.dat","w");
+    for(int i=0;i<sz;i++)
+    {
+        fprintf(fo,"*\n");
+        fprintf(fo,"%d\n",machines[i].code);
+        fprintf(fo,"%d\n",machines[i].repaircode);
+        fprintf(fo,"%d\n",machines[i].repairperiod);
+        fprintf(fo,"%c\n",machines[i].repairtype);
+        fprintf(fo,"%s",machines[i].repairdescription);
+    }
+    fclose(fo);
+    printf("Deleted Successfully \n");
 }
 
 void show_machine()
