70a7249be4c8ccb126eeaaa81afebb0375283102
Feri80 <bigfarhad7780@gmail.com>
2021-02-03 16:55:09 +0330

    add_machine added

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
 
 #define maxn 100
+int sz=0;
 
 struct machine
 {
@@ -11,6 +15,65 @@ struct machine
     char repairdescription[maxn];
 };
 
+void add_machine()
+{
+    struct machine new_machine;
+    int x;
+    do
+    {
+        printf("Please Enter Your Machine Code : \n1)Taraash\n2)Ferez\n3)Sang\n4)Drill\n5)Safhe Taraash\n6)Joosh\n7)Press\n8)Boresh\n9)Navar\n");
+        scanf("%d",&x);
+    }while(x>9 || x<1);
+    new_machine.code=x;
+    char tmp[1];
+    gets(tmp);
+    printf("Please Enter Your Machine Repair Description : \n");
+    gets(new_machine.repairdescription);
+    do
+    {
+        printf("Please Enter Your Machine Repair Code (1-4) : ");
+        scanf("%d",&x);
+    }while(x>4 || x<1);
+    new_machine.repaircode=new_machine.code*10+x;
+    do
+    {
+        printf("Please Enter Your Machine Repair Period (1-6) : ");
+        scanf("%d",&x);
+    }while(x>6 || x<1);
+    new_machine.repairperiod=x;
+    char c;
+    do
+    {
+        printf("Please Enter Your Machine Repair Type (M/E/I)/(m/e/i) : ");
+        scanf(" %c",&c);
+        c=toupper(c);
+    }while(c!='M' && c!='E' && c!='I');
+    new_machine.repairtype=c;
+    if(sz==0)
+    {
+        FILE *fo=fopen("machines.dat","w");
+        fprintf(fo,"*\n");
+        fprintf(fo,"%d\n",new_machine.code);
+        fprintf(fo,"%d\n",new_machine.repaircode);
+        fprintf(fo,"%d\n",new_machine.repairperiod);
+        fprintf(fo,"%c\n",new_machine.repairtype);
+        fprintf(fo,"%s\n",new_machine.repairdescription);
+        fclose(fo);
+    }
+    else
+    {
+        FILE *fo=fopen("machines.dat","a");
+        fprintf(fo,"*\n");
+        fprintf(fo,"%d\n",new_machine.code);
+        fprintf(fo,"%d\n",new_machine.repaircode);
+        fprintf(fo,"%d\n",new_machine.repairperiod);
+        fprintf(fo,"%c\n",new_machine.repairtype);
+        fprintf(fo,"%s\n",new_machine.repairdescription);
+        fclose(fo);
+    }
+    
+    sz++;
+}
 
 int main()
 {
