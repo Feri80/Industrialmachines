1b961070806c34803d3eb960902421c4129162e4
Feri80 <bigfarhad7780@gmail.com>
2021-02-03 15:50:37 +0330

    machine struct added

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+#define maxn 100
+
+struct machine
+{
+    int code;
+    int repaircode;
+    int repairperiod;
+    char repairtype;
+    char repairdescription[maxn];
+};
+
+
 int main()
 {
-    printf("Hello World !!!");
+    
 }
